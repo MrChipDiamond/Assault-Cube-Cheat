1252350a32283321708874032333d3974ac097e0
Chip Diamond <63815341+MrChipDiamond@users.noreply.github.com>
2024-08-15 14:24:48 -0400

    Adding updates slowly.
    This is built in C# ImGui

---
+++ AssaultCube Cheat/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using ClickableTransparentOverlay;
+using ImGuiNET;
+using System.Runtime.InteropServices;
+
+namespace AssaultCube_Cheat
+{
+    public class Renderer : Overlay
+    {
+        public bool godMode = false;
+        public bool unlimtedAmmo = false;
+        public bool unlimtedNades = false;
+        private bool showMenu = true;
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+
+        public Renderer()
+        {
+            var handle = GetConsoleWindow();
+            ShowWindow(handle, SW_HIDE);
+        }
+
+        protected override void Render()
+        {
+            if (ImGui.IsKeyPressed(ImGuiKey.Insert))
+            {
+                showMenu = !showMenu;
+            }
+
+            if (!showMenu) return;
+
+            ImGui.PushStyleColor(ImGuiCol.WindowBg, new Vector4(0.1f, 0.1f, 0.1f, 0.9f));
+            ImGui.PushStyleColor(ImGuiCol.TitleBg, new Vector4(0.2f, 0.2f, 0.8f, 1.0f));
+            ImGui.PushStyleColor(ImGuiCol.TitleBgActive, new Vector4(0.3f, 0.3f, 1.0f, 1.0f));
+            ImGui.PushStyleColor(ImGuiCol.Button, new Vector4(0.2f, 0.6f, 0.8f, 1.0f));
+            ImGui.PushStyleColor(ImGuiCol.ButtonHovered, new Vector4(0.3f, 0.7f, 0.9f, 1.0f));
+            ImGui.PushStyleColor(ImGuiCol.ButtonActive, new Vector4(0.1f, 0.5f, 0.7f, 1.0f));
+
+            Vector2 windowSize = new Vector2(520, 300);
+            ImGui.SetNextWindowSize(windowSize);
+            ImGui.Begin("Assault Cube");
+
+            if (ImGui.BeginTabBar("OptionsTabBar"))
+            {
+                if (ImGui.BeginTabItem("Main"))
+                {
+                    ImGui.Text("Basic Cheats");
+                    ImGui.Checkbox("Enable God Mode", ref godMode);
+                    ImGui.Checkbox("Enable Unlimited Ammo", ref unlimtedAmmo);
+                    ImGui.Checkbox("Enable Unlimited Gernades", ref unlimtedNades);
+                    ImGui.Separator(); 
+                    ImGui.EndTabItem();
+                }
+
+                if (ImGui.BeginTabItem("ESP"))
+                {
+                    ImGui.Separator(); 
+                    ImGui.EndTabItem();
+                }
+
+                ImGui.EndTabBar();
+            }
+
+            if (ImGui.Button("Exit"))
+            {
+                Environment.Exit(0); 
+            }
+
+            ImGui.End();
+        }
+    }
+}
---
+++ AssaultCube Cheat/Renderer.cs
@@ -0,0 +1,48 @@
+﻿using AssaultCube_Cheat;
+using Swed64;
+
+Renderer renderer = new Renderer();
+renderer.Start().Wait();
+
+Swed swed = new Swed ("ac_client");
+IntPtr moduleBase = swed.GetModuleBase("ac_client.exe");
+
+
+//ac_client.exe+C73EF - FF 08 <---Ammo   
+//ac_client.exe+1C223 - 29 73 04 <---health 
+//ac_client.exe+C7E5D - FF 08 <--- Gernades               
+
+IntPtr ammoAddress = moduleBase + 0xC73EF;
+IntPtr healthAddress = moduleBase + 0x1C223;
+IntPtr nadeAddress = moduleBase + 0xC7E5D;
+
+while (true)
+{
+    if (renderer.godMode)
+    {
+        swed.WriteBytes(healthAddress, "90 90 90"); //Enable gMode
+    }
+    else
+    {
+        swed.WriteBytes(healthAddress, "29 73 04"); //Disable gMode
+    }
+    //---
+    if (renderer.unlimtedAmmo) 
+    {
+        swed.WriteBytes(ammoAddress, "90 90"); //Enable Ammo
+    }
+    else
+    {
+        swed.WriteBytes(ammoAddress, "FF 08"); //Disable Ammo
+    }
+    if (renderer.unlimtedNades)
+    {
+        swed.WriteBytes(nadeAddress, "90 90"); //Enable Gernades
+    }
+    else
+    {
+        swed.WriteBytes(nadeAddress, "FF 08"); //Disable Gernades
+    }
+    //---
+
+}
